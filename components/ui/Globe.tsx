6a097c18ada1cf6cd09c558dcac91853f5ddd2eb
vincent.bharata <vincent.bharata@phincon.com>
2025-02-09 14:11:45 +0700

    update

--- components/ui/Globe.tsx
+++ components/ui/Globe.tsx
@@ -234,7 +234,7 @@ export function WebGLRendererConfig() {
   const { gl, size } = useThree();
 
   useEffect(() => {
-    gl.setPixelRatio(window.devicePixelRatio);
+    gl.setPixelRatio(git.devicePixelRatio);
     gl.setSize(size.width, size.height);
     gl.setClearColor(0xffaaff, 0);
   }, []);

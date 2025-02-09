08dbb89b8e667d97ec2153d1f4db98a739f95213
vincent.bharata <vincent.bharata@phincon.com>
2025-02-09 15:10:48 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -21,9 +21,5 @@ export default withSentryConfig(
     hideSourceMaps: true,
     disableLogger: true,
     automaticVercelMonitors: true,
-
-    // Menonaktifkan plugin Webpack untuk menghindari upload source maps
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
   }
 );
